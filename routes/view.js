d4539016161b9bb77ce4bce605201882e1cdcbbd
Matthew Guillen <matthewguillen777@gmail.com>
2021-03-20 19:46:45 -0400

    edit routes info

--- routes/view.js
+++ routes/view.js
@@ -1,11 +1,11 @@
 const path = require("path");
 const router = require("express").Router();
 
-// Route to notes page
+// Route to portfolio page
 router.get("/portfolio", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/portfolio.html"));
 });
-// Route to notes page
+// Route to contact page
 router.get("/contact", function(req, res) {
   res.sendFile(path.join(__dirname, "../public/contact.html"));
 });
